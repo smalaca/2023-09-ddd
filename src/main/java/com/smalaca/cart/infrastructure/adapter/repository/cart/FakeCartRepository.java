3967d5647142c716a8a04c9a6ca47a853e57b276
smalaca <ab13.krakow@gmail.com>
2023-09-12 16:48:57 +0200

    add FakeCartRepository

---
+++ src/main/java/com/smalaca/cart/infrastructure/adapter/repository/cart/FakeCartRepository.java
@@ -0,0 +1,18 @@
+package com.smalaca.cart.infrastructure.adapter.repository.cart;
+
+import com.smalaca.cart.command.domain.cart.Cart;
+import com.smalaca.cart.command.domain.cart.CartRepository;
+
+import java.util.UUID;
+
+public class FakeCartRepository implements CartRepository {
+    @Override
+    public Cart findById(UUID cartId) {
+        return null;
+    }
+
+    @Override
+    public void save(Cart cart) {
+
+    }
+}
