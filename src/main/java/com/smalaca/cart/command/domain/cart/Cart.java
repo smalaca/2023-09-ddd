927725de48e9aa176577a25a7957e443a61b6615
smalaca <ab13.krakow@gmail.com>
2023-09-13 10:29:25 +0200

    add DDD annotations

--- src/main/java/com/smalaca/cart/command/domain/cart/Cart.java
+++ src/main/java/com/smalaca/cart/command/domain/cart/Cart.java
@@ -1,11 +1,14 @@
 package com.smalaca.cart.command.domain.cart;
 
+import com.smalaca.annotations.ddd.AggregateRoot;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.UUID;
 
 @Entity
+@AggregateRoot
 public class Cart {
     @Id
     @GeneratedValue
--- src/main/java/com/smalaca/cart/command/domain/cart/CartRepository.java
+++ src/main/java/com/smalaca/cart/command/domain/cart/CartRepository.java
@@ -1,7 +1,10 @@
 package com.smalaca.cart.command.domain.cart;
 
+import com.smalaca.annotations.ddd.DomainRepository;
+
 import java.util.UUID;
 
+@DomainRepository
 public interface CartRepository {
     Cart findById(UUID cartId);
 
