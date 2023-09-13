0ef6a186fe81638eafa931ccf906da6476fded76
smalaca <ab13.krakow@gmail.com>
2023-09-13 10:08:19 +0200

    add javax annotation over Cart

--- src/main/java/com/smalaca/cart/command/domain/cart/Cart.java
+++ src/main/java/com/smalaca/cart/command/domain/cart/Cart.java
@@ -1,6 +1,16 @@
 package com.smalaca.cart.command.domain.cart;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
 public class Cart {
+    @Id
+    @GeneratedValue
+    private UUID cartId;
+
     public void addProduct() {
 
     }
--- src/main/java/com/smalaca/cart/infrastructure/adapter/repository/cart/SpringDataJpaCartRepository.java
+++ src/main/java/com/smalaca/cart/infrastructure/adapter/repository/cart/SpringDataJpaCartRepository.java
@@ -2,17 +2,20 @@ package com.smalaca.cart.infrastructure.adapter.repository.cart;
 
 import com.smalaca.cart.command.domain.cart.Cart;
 import com.smalaca.cart.command.domain.cart.CartRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.UUID;
 
 public class SpringDataJpaCartRepository implements CartRepository {
+    private CrudRepository<Cart, UUID> repository;
+
     @Override
     public Cart findById(UUID cartId) {
-        return null;
+        return repository.findById(cartId).get();
     }
 
     @Override
     public void save(Cart cart) {
-
+        repository.save(cart);
     }
 }
